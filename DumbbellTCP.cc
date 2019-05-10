9f2986dfd7756f77d80e0acc4ec8f9d0f2d9d817
pandari260 <pandari260@gmail.com>
2019-05-10 13:20:21 -0300

    se genera el Pcap

--- DumbbellTCP.cc
+++ DumbbellTCP.cc
@@ -1,5 +1,4 @@
 #include <iostream>
-
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
 #include "ns3/internet-module.h"
@@ -10,13 +9,17 @@
 #include "ns3/tcp-option-winscale.h"
 #include "ns3/udp-socket-factory.h"
 #include "ns3/random-variable-stream.h"
+#include "ns3/tcp-socket-base.h"
 
 using namespace ns3;
 
+
 int main (int argc, char *argv[]){
-  Config::SetDefault ("ns3::OnOffApplication::PacketSize", UintegerValue (100));
+  Config::SetDefault ("ns3::OnOffApplication::PacketSize", UintegerValue (512));
   Config::SetDefault ("ns3::OnOffApplication::DataRate", StringValue ("500kb/s"));
 
+
+  
   uint32_t    nLeftLeaf = 5;
   uint32_t    nRightLeaf = 5;
   uint32_t    nLeaf = 0; // If non-zero, number of both left and right
@@ -66,15 +69,18 @@ int main (int argc, char *argv[]){
 
   uint16_t puerto=50000;
 
-
-  PacketSinkHelper sink ("ns3::TcpSocketFactory",
-                     InetSocketAddress (Ipv4Address::GetAny (), puerto));
+  PacketSinkHelper sink ("ns3::TcpSocketFactory", InetSocketAddress (Ipv4Address::GetAny (), puerto));
 
   ApplicationContainer appsReceptor;
   appsReceptor.Add(sink.Install(redDumbbell.GetLeft(0)));
-  appsReceptor.Add(sink.Install(redDumbbell.GetLeft(1))); 
+  //appsReceptor.Add(sink.Install(redDumbbell.GetLeft(1))); 
   appsReceptor.Start (Seconds (0.0));
   appsReceptor.Stop (Seconds (1.0));
+
+  //Ptr<Socket> ns3TcpSocket1 = Socket::CreateSocket (redDumbbell.GetLeft(0), TcpSocketFactory::GetTypeId ());
+  //ns3TcpSocket1->TraceConnectWithoutContext ("CongestionWindow", MakeCallback (&CwndChange));
+
+
 
 
   PacketSinkHelper sinkUdp ("ns3::UdpSocketFactory",  InetSocketAddress (Ipv4Address::GetAny (), puerto));
@@ -102,7 +108,7 @@ int main (int argc, char *argv[]){
 
       AddressValue remoteAddress1 (InetSocketAddress (redDumbbell.GetLeftIpv4Address (1), puerto));
       emisorHelper.SetAttribute ("Remote", remoteAddress1);
-      appEmisor.Add (emisorHelper.Install (redDumbbell.GetRight (1)));
+      //appEmisor.Add (emisorHelper.Install (redDumbbell.GetRight (1)));
 
       AddressValue remoteAddress2 (InetSocketAddress (redDumbbell.GetLeftIpv4Address (2), puerto));
       emisorHelperUdp.SetAttribute ("Remote", remoteAddress2);
@@ -111,18 +117,24 @@ int main (int argc, char *argv[]){
       appEmisor.Start(Seconds(0.0));
       appEmisor.Stop(Seconds(1.0));
     // Set the bounding box for animation
-    redDumbbell.BoundingBox (1, 1, 100, 100);
 
+
+  AsciiTraceHelper ascii;
+  p2pReceptor.EnableAsciiAll (ascii.CreateFileStream ("dumbbellPCAP/dumbellR.tr"));
+  p2pReceptor.EnablePcapAll ("dumbbellPCAP/dumbellR");
+
+
+    redDumbbell.BoundingBox (1, 1, 100, 100);
     // Create the animation object and configure for specified output
     AnimationInterface anim (animFile);
     anim.EnablePacketMetadata (); // Optional
     anim.EnableIpv4L3ProtocolCounters (Seconds (0), Seconds (10)); // Optional
-
     // Set up the actual simulation
   Ipv4GlobalRoutingHelper::PopulateRoutingTables ();
-  
+
   Simulator::Run ();
   std::cout << "Animation Trace file created:" << animFile.c_str ()<< std::endl;
+  std::cout << "RTT: " << tshark -r dumbbellPCAP/dumbbellR.pcap -Y 'ip.addr == AA.BB.CC.DD' -T fields -e tcp.analysis.ack_rtt
   Simulator::Destroy ();
   return 0;
 
